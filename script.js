014f36e3c2c90db7fc1fa4b46b123e986956e05a
abbeym <78575085+Tech-A@users.noreply.github.com>
2023-09-10 18:57:30 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const map = L.map('map').setView([51.505, -0.09], 15);
 
 
 // Replace 'image-url' with the path to your exported PNG map image
-const imageUrl = 'MapLevel1.png';
+const imageUrl = 'Artboard 5.png';
 const mapBounds = map.getBounds();
 const imageBounds = [[mapBounds._southWest.lat, mapBounds._southWest.lng], [mapBounds._northEast.lat, mapBounds._northEast.lng]];
 
@@ -14,4 +14,28 @@ L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Add the PNG image overlay tosc the map
 //L.imageOverlay(imageUrl, imageBounds).addTo(map);
+//const pixelX = 100; // Replace with your pixel X-coordinate
+//const pixelY = 150; // Replace with your pixel Y-coordinate
 
+// Add a marker at the provided pixel coordinates
+//const marker = L.marker(map.unproject([pixelX, pixelY], map.getMaxZoom())).addTo(map);
+
+L.marker([51.501958, -0.073497]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+L.marker([51.508101, -0.075574]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
